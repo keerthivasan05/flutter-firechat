ec291cc24ee012d0c7cf51edcf187fadd1146980
keerthivasan05 <72241111+keerthivasan05@users.noreply.github.com>
2020-10-06 10:08:41 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:fire_chat/screens/welcome_screen.dart';
+import 'package:fire_chat/screens/login_screen.dart';
+import 'package:fire_chat/screens/registration_screen.dart';
+import 'package:fire_chat/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+       initialRoute:WelcomeScreen.id,
+       routes:{
+         WelcomeScreen.id:(context)=>WelcomeScreen(),
+        LoginScreen.id:(context)=>LoginScreen(),
+        RegistrationScreen.id:(context)=>  RegistrationScreen(),
+         ChatScreen.id:(context)=>ChatScreen(),
+       }
+      
+    );
+  }
+}
